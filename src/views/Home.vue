3a2df30471a3a7f4eb81d4e0d08344077e87ac94
eno <muhammadrisano@gmail.com>
2020-06-09 07:38:02 +0700

    coba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{todo.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      todos: []
+    }
+  },
+  mounted () {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        this.todos = res.data
+      })
   }
 }
 </script>
